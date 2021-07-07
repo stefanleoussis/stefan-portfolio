360a7ba7489f74a2e7d22df526a03f47a1ca5c11
Stefan <stefanleoussis@gmail.com>
2021-07-07 13:56:43 -0400

    Created Project Cards

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import "./App.css";
+import { Grid } from "@material-ui/core";
+import ProjectCard from "./components/ProjectCard";
+import img1 from "./images/img1.jpg";
+import img2 from "./images/img2.jpg";
+import img3 from "./images/img3.jpg";
 
 function App() {
   return (
     <div className="App" style={{ fontSize: 20 }}>
-      Hello
+      <Grid container direction="row" justifyContent="space-between">
+        <ProjectCard title="Messenger" imgURL={img1} />
+        <ProjectCard title="Chatbot" imgURL={img2} />
+        <ProjectCard title="Web Api" imgURL={img3} />
+      </Grid>
     </div>
   );
 }
